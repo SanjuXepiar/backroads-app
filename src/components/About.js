750eb1b94f9a6c7af858de2498d8ccc5800fdc6e
SanjuXepiar <sanjusarkar238@gmail.com>
2024-09-10 04:25:15 +0530

    updated dependencies

--- src/components/About.js
+++ src/components/About.js
@@ -25,9 +25,9 @@ export const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          {/* <a href="./" className="btn">
             read more
-          </a>
+          </a> */}
         </article>
       </div>
     </section>
